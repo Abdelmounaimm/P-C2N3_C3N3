e0c3dffe7e80286071166a4c25c9f23c4d666c50
elgana <email@gmail.com>
2020-05-14 21:39:35 +0000

    testBtn

--- js/script.js
+++ js/script.js
@@ -1,61 +1,27 @@
-const questionnaire =[
-    {
-        "Q":'sii',
-        "element":["label", ],
-        "text":["age"],
-        "input":"input",
-        "type":'radio',
-        "value":[],
-        "name":"Q3",
-        "R":""
-    },
-    {
-        "Q":'Pensez-vous avoir ou avoir eu de la fièvre ces 10 derniers jours (frissons, su' +
-        'eurs) ?',
-        "element":["label", "label"],
-        "text": ["NON", "OUI"]
-        "input": "input",
-        "type":'radio',
-        "value":[1,2],
-        "name":"Q1",
-        "R": ""
-    },
-    {
-        "Q":'',
-        "element":["label","label","label","label"],
-        "text":[],
-        "input":"input",
-        "type":"radio",
-        "value":[1, 2, 1, 1],
-        "name":"Q2",
-        "R":""
-    },
-    {
-        "Q":'sii',
-        "element":["label", ],
-        "text":["age"],
-        "input":"input",
-        "type":'radio',
-        "value":[],
-        "name":"Q3",
-        "R":""
-    },
-];
-//////selector/////
-const testBtn = document.querySelector('.start-btn')
-const questionnaire = document.querySelector('.questionnaire')
-const Préambule = document.querySelector('.Préambule')
+// DOM
+
+const testBtn = document.querySelector('.start-btn')//
+const questionnaire = document.querySelector('.questionnaire')//
+const Préambule = document.querySelector('.Préambule')//
 const stepper = document.querySelectorAll('.stepper h1')
-const nextBtn = document.querySelector('.next')
-const previousBtn = document.querySelector('.previous')
-const currentquestion = document.querySelector('.question')
+const nextBtn = document.querySelector('.next')//
+const previousBtn = document.querySelector('.previous')//
+const currentQuestion = document.querySelector('.question')
 const answerInputs = document.querySelector('.answer-inputs')
 const progressBar = document.querySelector('.bar')
 const questionNumber = document.querySelector('.question-number')
 const animateBox = document.querySelector('.animation')
 
 
+//      Event Listener
 
-counter = -1;
 
-console.log(resultMessage[0]);
+testBtn.addEventListener('click', startTest)
+
+function startTest() {
+    testBtn.style.display = 'none'
+    Préambule.style.display = 'none'
+    questionnaire.style.display = 'block'
+
+}
+
